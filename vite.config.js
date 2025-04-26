62c6c4dd8b9dcc7a6fcaa8e061768e56b1f28873
PabloG-7 <pablooliver853@gmail.com>
2025-04-26 08:41:27 -0300

    update

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/pokedex/', // <- isso aqui é essencial!
   plugins: [react()],
+  base: './', // <-- ESSA LINHA É IMPORTANTE
 })
