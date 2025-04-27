f9c54760b782cb956b5a42e1e3093b81bb4cc731
PabloG-7 <pablooliver853@gmail.com>
2025-04-27 10:56:09 -0300

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // <-- ESSA LINHA É IMPORTANTE
-})
+  base: '/pokedex/',  // Ajuste o base path para o subdiretório do GitHub Pages
+});
